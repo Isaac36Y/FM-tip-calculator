78cbd43c3a7d0c06d00c66a89cdd1ad9d1119a5e
Isaac Young <isaac3.young6@gmail.com>
2026-01-13 21:21:45 -0800

    added reset button function and some hover stylings. made desktop media query

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const customTip = document.querySelector(".tip__input")
 const tipAmount = document.querySelectorAll(".tip__button")
 const totalTip = document.querySelector("#total-tip-per-person")
 const totalPerson = document.querySelector("#total-cost-per-person")
-
+const resetBtn = document.querySelector("#reset-button")
 
 
 let tipSelected = null
@@ -15,21 +15,36 @@ const parsePercent = (str) => {
     return parseFloat(aStr)
 }
 
+const activateReset = () => {
+    if (tipSelected || billInput.value || numOfPeople.value) {
+        return resetBtn.classList.add("select")
+    }else {return resetBtn.classList.remove("select")}
+}
+
+const reset = () => {
+    tipSelected = null;
+    billInput.value = "";
+    numOfPeople.value = "";
+    customTip.value = ""
+    tipAmount.forEach(btn => btn.ariaChecked = "false")
+    resetBtn.classList.remove("select")
+}
+
 const updateTotals = () => {
     const bill = parseFloat(billInput.value);
     const people = parseInt(numOfPeople.value);
     const percent = parsePercent(tipSelected)
-
-    if (!people >= 1) {
+    activateReset()
+    if (!people >= 1 || !bill) {
         totalTip.textContent = "$0.00"
         totalPerson.textContent = "$0.00"
         return
     }
-
+    
     const tipTotal = bill * (percent / 100);
     const tipPerPerson = tipTotal / people;
     const totalPerPerson = (bill + tipTotal) / people
-
+    
     totalTip.textContent = tipPerPerson.toFixed(2);
     totalPerson.textContent = totalPerPerson.toFixed(2)
 }
@@ -45,6 +60,7 @@ tipAmount.forEach(btn =>
        btn.ariaChecked = "true"
        tipSelected = parsePercent(btn.textContent)
        if (customTip) customTip.value = ""
+       activateReset()
     }
     if (billInput.value && numOfPeople.value >= 1) {
          updateTotals()
@@ -64,4 +80,5 @@ if (customTip ) {
 }
 billInput.addEventListener("input", () => updateTotals())
 numOfPeople.addEventListener("input", () => updateTotals())
+resetBtn.addEventListener("click", () => reset())
 
